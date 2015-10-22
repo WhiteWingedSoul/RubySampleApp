f6a913af6e47725ce18878c9aaa8c5d469aa5815
White <cooro1994@gmail.com>
2015-10-22 12:42:08 +0000

    add view users index, delete user function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,35 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
   def new
+    if logged_in?
+      redirect_to root_path
+    end
+    
     @user = User.new
   end
   
   def show
     @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated !"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
   
   def create
@@ -18,9 +43,33 @@ class UsersController < ApplicationController
     end
   end
   
-  private 
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Successful deleted user !"
+    redirect_to users_path
   end
   
+  private 
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in to access this page"
+        redirect_to login_path
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_path unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to root_path unless current_user.admin?
+    end
+    
 end
