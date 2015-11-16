6ea947701d48e14201e13805a6661ec9203bbe80
White <cooro1994@gmail.com>
2015-11-16 15:12:28 +0000

    add account activation feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,9 +36,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Registration successful!"
-      redirect_to root_path
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
