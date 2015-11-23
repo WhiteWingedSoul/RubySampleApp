d856afa5da53149e6984bdab8a82c62acda97967
White <cooro1994@gmail.com>
2015-11-23 15:52:59 +0000

    add micropost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def edit
@@ -54,14 +55,6 @@ class UsersController < ApplicationController
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
-    
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in to access this page"
-        redirect_to login_path
-      end
     end
     
     def correct_user
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,20 @@ User.create!(name:  "Example User",
              activated: true,
              activated_at: Time.zone.now)
 
+User.create!(name:  "Hoang Viet",
+             email: "cooro1994@yahoo.com",
+             password:              "738798",
+             password_confirmation: "738798",
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
+
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
